553ec5ddd553058e18aac53027aea53b9504e1b6
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2025-09-20 13:25:37 +0600

    JWT api

--- 3. JWT/app.js
+++ 3. JWT/app.js
@@ -1,6 +1,47 @@
 const express = require('express');
 const app = express();
 const port = 3000;  
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const JSON_SECRET_KEY = "876823gbj";
+
+app.use(bodyParser.json());
+
+const data = {
+    "tanvir":{password:"1234"},
+    "admin":{password:"1234"}
+}
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running');
+});
+
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    if(!username || !password){
+        return res.status(400).send("Invalid username or password");
+    } else {
+        if(data[username] && data[username].password === password){
+            const token = jwt.sign({username}, JSON_SECRET_KEY, { expiresIn: '1h' });
+            return res.json({token});
+        } else {
+            return res.status(401).send("Authentication failed");
+        }
+    }
+})
+
+app.get('/protected', (req, res) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    console.log("Received Token:", token);
+    if (!token) {
+        return res.status(401).send("Access denied. No token provided.");
+    }
+
+    res.send(`Protected data accessed with token: ${token}`);
+
+});
+
 
 
 
