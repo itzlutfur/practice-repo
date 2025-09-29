5c838ab6829a3ccf422535f542ead6363c2fbfd9
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2025-09-30 00:52:46 +0600

    fullstack structure

---
+++ 5. FullStack Intro/backend/src/index.ts
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.post("/data", (req: Request, res: Response) => {
+  const { name, email, description } = req.body;
+  
+  res.status(200).json({
+    message: `${name}`,
+    email: `${email}`,
+    description: `${description}`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
